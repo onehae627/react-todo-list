b7374c311f4cc965fc62ec9fd260d5a7f0494148
onehae627 <onehae627@gmail.com>
2023-02-14 20:51:03 +0900

    탭메뉴에 따라 필터링 처리

--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -1,20 +1,72 @@
+import { Tab, Tabs } from "@mui/material";
+import { useRecoilState } from "recoil";
+
 import { useTodosStatus, useTodoOptionDrawerStatus } from "../hooks";
 import TodoOptionDrawer from "../components/TodoOptionDrawer";
 import TodoListItem from "../components/TodoListItem";
-
+import { TodoList__filterCompletedIndexAtom } from "../atoms";
 
 export default function TodoList() {
     const todosStatus = useTodosStatus();
     const todoOptionDrawerStatus = useTodoOptionDrawerStatus();
     const onCompletedBtnClicked = (id) => todosStatus.toggleTodoCompletedById(id);
-    
+    const [filterCompletedIndex, setFilterCompletedIndex] = useRecoilState(
+        TodoList__filterCompletedIndexAtom
+      );
+
+    const getFilteredTodos = () => {
+        if ( filterCompletedIndex == 1) {
+            return todosStatus.todos.filter((todo)=> !todo.completed);
+        }
+        if ( filterCompletedIndex == 2) {
+            return todosStatus.todos.filter((todo)=> todo.completed);
+        }
+        return todosStatus.todos;
+    };
+
+    const filteredTodos = getFilteredTodos();
+
   
     return (
       <>
         <TodoOptionDrawer status={todoOptionDrawerStatus} />
+
+        <Tabs
+        variant="fullWidth"
+        value={filterCompletedIndex}
+        onChange={(event, newValue) => setFilterCompletedIndex(newValue)}
+      >
+        <Tab
+          label={
+            <span className="flex">
+              <i className="fa-solid fa-list-ul"></i>
+              <span className="ml-2">전체</span>
+            </span>
+          }
+          value={0}
+        />
+        <Tab
+          label={
+            <span className="flex">
+              <i className="fa-regular fa-square"></i>
+              <span className="ml-2">미완료</span>
+            </span>
+          }
+          value={1}
+        />
+        <Tab
+          label={
+            <span className="flex">
+              <i className="fa-regular fa-square-check"></i>
+              <span className="ml-2">완료</span>
+            </span>
+          }
+          value={2}
+        />
+      </Tabs>
         <div className="mt-4 px-4">
           <ul>
-            {todosStatus.todos.map((todo, index) => (
+            {filteredTodos.map((todo, index) => (
               <TodoListItem
                 key={todo.id}
                 todo={todo}
