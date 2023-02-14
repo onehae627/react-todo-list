93fc3c66e90db14d8f14dbe2131a8bd0028f7677
onehae627 <onehae627@gmail.com>
2023-02-14 21:07:37 +0900

    할 일 수정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route, Navigate, useLocation, NavLink } from "react-router-dom"
 import MainPage from "./pages/MainPage";
 import WritePage from "./pages/WritePage";
 import { NoticeSnackbar } from "./components/NoticeSnackbar";
+import EditPage from "./pages/EditPage";
 
 
 function App() {
@@ -17,9 +18,9 @@ function App() {
           <span className="font-bold select-none">앱 이름</span>
           <div className="flex-1 flex justify-end">
             {location.pathname != "/write" &&
-            <NavLink to="/write">글쓰기</NavLink>}
+            <NavLink to="/write">할 일 추가</NavLink>}
             {location.pathname == "/write" &&
-            <NavLink to="/main">메인</NavLink>}
+            <NavLink to="/main">리스트</NavLink>}
           </div>
         </Toolbar>
       </AppBar>
@@ -27,6 +28,7 @@ function App() {
       <Routes>
         <Route path="/main" element={<MainPage />} />
         <Route path="/write" element={<WritePage />} />
+        <Route path="/edit/:id" element={<EditPage/>} />
         <Route path="*" element={<Navigate to="/main" />} />
       </Routes>
     </>
