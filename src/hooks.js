24c08d864d9970b43caa249fcf3d5b028d166699
onehae627 <onehae627@gmail.com>
2023-02-14 21:31:44 +0900

    할 일 수정

--- src/hooks.js
+++ src/hooks.js
@@ -42,21 +42,22 @@ export function useTodosStatus() {
     return id;
   };
 
-  const modifyTodo = (index, newContent) => {
-    const newTodos = todos.map((todo, _index) =>
-      _index != index ? todo : { ...todo, content: newContent }
-    );
+  const modifyTodo = (index, content, regDate) => {
+    const newTodos =produce(todos, (draft) => {
+      draft[index].regDate = dateToStr(new Date(regDate));
+      draft[index].content = content;
+    });
     setTodos(newTodos);
   };
 
-  const modifyTodoById = (id, newContent) => {
+  const modifyTodoById = (id, newContent, regDate) => {
     const index = findTodoIndexById(id);
 
     if (index == -1) {
       return;
     }
 
-    modifyTodo(index, newContent);
+    modifyTodo(index, newContent, regDate);
   };
 
   const removeTodo = (index) => {
--- src/pages/EditPage.js
+++ src/pages/EditPage.js
@@ -1,49 +1,75 @@
 import { TextField, Button } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
 import { useNoticeSnackbarStatus } from "../components/NoticeSnackbar";
 import { useTodosStatus } from "../hooks";
 
 export default function EditPage() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
   const noticeSnackbarStatus = useNoticeSnackbarStatus();
-  const todosStatus= useTodosStatus();
+  const todosStatus = useTodosStatus();
+
+  const todo = todosStatus.findTodoById(id);
+
   const onSubmit = (e) => {
     e.preventDefault();
 
     const form = e.target;
 
     if (form.regDate.value.length == 0) {
-      alert("날짜를 입력해주세요!");
-
+      alert("날짜를 입력해주세요.");
       form.regDate.focus();
 
       return;
     }
 
     if (form.content.value.length == 0) {
-      alert("내용을 입력해주세요!");
-
+      alert("내용을 입력해주세요.");
       form.content.focus();
 
       return;
     }
+
     const newTodoId = todosStatus.addTodo(
       form.regDate.value,
       form.content.value
     );
 
-    noticeSnackbarStatus.open(`${newTodoId}번 할일이 추가되었습니다.`);
+    noticeSnackbarStatus.open(`${newTodoId}번 할 일이 수정되었습니다.`);
+
+    navigate(-1);
   };
-  
-    return (
-      <>
-        <form className="flex-1 flex p-10 flex-col gap-7" onSubmit={onSubmit}>            
-                <TextField label="언제 해야 하나요?" focused type="datetime-local" name="regDate"/>
-                <TextField name="content" label="무엇을 해야 하나요?" className="flex-1 flex " InputProps={{className : "flex-1 flex-col "}} inputProps={{className : "flex-1"}} multiline/>            
-                <Button type="submit" variant="contained">
-                  <span><i className="fa-solid fa-pen"></i></span>
-                  <span>&nbsp;</span>
-                  <span>할 일 수정</span>
-                </Button>
-        </form>
-      </>
-    );
-  }
+
+  const regDateForInput = todo.regDate.substr(0, 16).replace(" ", "T");
+
+  return (
+    <>
+      <form className="flex-1 flex p-10 flex-col gap-7" onSubmit={onSubmit}>
+        <TextField
+          label="언제 해야 하나요?"
+          focused
+          type="datetime-local"
+          name="regDate"
+          defaultValue={regDateForInput}
+        />
+        <TextField
+          name="content"
+          label="무엇을 해야하나요?"
+          className="flex-1 flex"
+          InputProps={{ className: "flex-1 flex-col" }}
+          inputProps={{ className: "flex-1" }}
+          multiline
+          defaultValue={todo.content}
+        />
+        <Button type="submit" variant="contained">
+          <span>
+            <i className="fa-solid fa-pen"></i>
+          </span>
+          <span>&nbsp;</span>
+          <span>할 일 수정</span>
+        </Button>
+      </form>
+    </>
+  );
+}
